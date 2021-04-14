76f66beff72e8090f6ea0a5c18d1b31d6c926284
rezyfr <frizkillah98@gmail.com>
2021-04-14 13:25:50 +0700

    modify gradle

--- app/build/generated/data_binding_trigger/debug/com/rezyfr/dicoding/made/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.rezyfr.dicoding.made;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
--- app/build/generated/source/kapt/debug/androidx/databinding/DataBinderMapperImpl.java
+++
@@ -1,8 +0,0 @@
-package androidx.databinding;
-
-public class DataBinderMapperImpl extends MergedDataBinderMapper {
-  DataBinderMapperImpl() {
-    addMapper(new com.rezyfr.dicoding.made.DataBinderMapperImpl());
-    addMapper("com.rezyfr.dicoding.favmovie");
-  }
-}
